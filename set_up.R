0ff6063990754b86bcfb02c9fb72a543bc0bcdeb
Elibeth Cirilo <e.cirilo@pucp.pe>
2021-11-05 09:22:03 -0500

    Revert "#34 ML_wage_prediction done"
    This reverts commit 4e0b4e53a330b09c5952eb01cb7691e5656e836d.

--- set_up.R
+++
@@ -1,6 +0,0 @@
-# tensorflow env 
-Sys.setenv(RETICULATE_PYTHON = "C:\\Users\\Elibeth\\anaconda3\\envs\\tenskeras\\python.exe")
-
-# anothet way to set tensorflow env
-# library(reticulate)
-# use_condaenv(condaenv = "tenskeras", required = T)
